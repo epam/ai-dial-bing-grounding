ff821e7618bac116140f3bc564ae9256855aab02
Daniil Yarmalkevich <daniil_yarmalkevich@epam.com>
2025-05-21 10:04:15 +0300

    formatting citations

--- aidial_bing_grounding/agent/event_handler.py
+++ aidial_bing_grounding/agent/event_handler.py
@@ -53,9 +53,7 @@ class EventHandler(AsyncAgentEventHandler[DialException | None]):
                     for idx, (title, url) in enumerate(
                         new_citations, start=len(self.citations) + 1
                     ):
-                        self.choice.append_content(
-                            f"<sup>[ {idx} ]({url})</sup>"
-                        )
+                        self.choice.append_content(f"[\[{idx}\]]({url})")
                         self.choice.add_attachment(
                             data="",
                             title=title,
