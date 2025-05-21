f043e5f513d6ac8e102c55a240e9fc15e318a996
Daniil Yarmalkevich <daniil_yarmalkevich@epam.com>
2025-05-21 09:27:57 +0300

    format

--- aidial_bing_grounding/agent/event_handler.py
+++ aidial_bing_grounding/agent/event_handler.py
@@ -55,7 +55,10 @@ class EventHandler(AsyncAgentEventHandler[DialException | None]):
                     ):
                         self.choice.append_content(f" [{idx}]({url})")
                         self.choice.add_attachment(
-                            data="", title=title, reference_url=url, type="text/markdown"
+                            data="",
+                            title=title,
+                            reference_url=url,
+                            type="text/markdown",
                         )
                     self.citations.extend(new_citations)
                 else:
